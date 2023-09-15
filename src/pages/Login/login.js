552085e920fa2218131638712745b907097a751f
IT_Winner <ronaldhaupt0503@gmail.com>
2023-09-14 17:56:47 -0700

    Update login.js

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -8,7 +8,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
   const methods = useForm();
-  const baseURL = process.env.REACT_APP_API_URL | +"login";
+  const baseURL = process.env.REACT_APP_API_URL + "login";
   const dispatch = useDispatch();
 
   const onSubmit = methods.handleSubmit((data) => {
