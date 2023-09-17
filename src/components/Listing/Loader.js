d88d183ba24787b4fd113215a3be5b13dfba6dbe
IT_Winner <ronaldhaupt0503@gmail.com>
2023-09-17 06:07:31 -0700

    fix: loader

--- src/components/Listing/Loader.js
+++ src/components/Listing/Loader.js
@@ -7,8 +7,10 @@ const Loader = () => {
         <div className="h-4 w-4 rounded-full bg-blue-500 animate-bounce"></div>
       </div>
       <div className="mt-4 text-center">
-        <p className="text-lg font-semibold text-gray-700">Loading...</p>
-        <p className="text-sm text-gray-500">Your data is on its way!</p>
+        <p className="text-lg font-semibold text-gray-700">
+          少しお待ちください。
+        </p>
+        {/* <p className="text-sm text-gray-500">少しお待ちください。</p> */}
       </div>
     </div>
   );
